be12fadb84fac8af1a149ef8d17cffd29280f845
hesham <hishamazmy2015@gmail.com>
2021-08-23 15:04:35 +0400

    join betwee two authors

--- src/Book.js
+++ src/Book.js
@@ -60,7 +60,11 @@ function Book({ book, MoveTo }) {
             </div>
           </div>
           <div className="book-title">{book.title}</div>
-          <div className="book-authors">{book.authors}</div>
+          <div className="book-authors">
+            {/* {book.authors} */}
+          {book.authors && book.authors.join(', ') }
+            
+            </div>
         </div>
       ) : (
         <div></div>
